45beddaa4431175d600e24a37587125b79ba874e
ianvexler <ianvexler@gmail.com>
2022-12-14 12:48:46 +0000

    Creating users is now possible.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "main#index"
+
+  # Sign up routes
+  get "sign_up", to: "users#new"
+  post "sign_up", to: "users#create"
+
+  # Login routes
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+
+  # Logout routes
+  post "logout", to: "sessions#delete"
+
 end
